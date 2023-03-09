cf6621da5c633172f809bbe69ddcc0f922d9faee
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-03-09 09:25:11 +0100

    Esercizio finito + bonus

---
+++ resources/views/features.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Features</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header class="text-bg-dark p-3 fs-5">
+
+        <div class="container-fluid w-75 mx-auto d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                I &hearts; Laravel
+            </div>
+
+            <nav class="navbar navbar-expand-lg">
+
+                <div class="container-fluid">
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link text-light" href="home">Home</a>
+                            <a class="nav-link active" aria-current="page" href="features">Features</a>
+                            <a class="nav-link text-light" href="contacts">Contacts</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </nav>
+
+        </div>
+
+    </header>
+
+    <main class="bg-black">
+
+        <div class="container-fluid w-75 mx-auto vh-100">
+            <h1 class="text-center text-white pt-5">Hello World pt2!</h1>
+            <h2 class="text-center text-white ">Welcome to my Features Page!</h2>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
